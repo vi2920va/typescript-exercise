3c9ebf0092d27d786718fb2a878094eb65ddec95
hyun <vi2920va@gmail.com>
2022-01-01 21:53:23 +0900

    Add: DOM 요소와 상호 작용

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
+// ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
-  form: HTMLFormElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -13,13 +17,37 @@ class ProjectInput {
       this.templateElement.content,
       true
     );
-    this.form = importedNode.firstElementChild as HTMLFormElement;
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    ) as HTMLInputElement;
+
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler);
+  }
+
   private attach() {
-    this.hostElement.insertAdjacentElement('afterbegin', this.form);
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
   }
 }
 
-const proInput = new ProjectInput();
+const prjInput = new ProjectInput();
